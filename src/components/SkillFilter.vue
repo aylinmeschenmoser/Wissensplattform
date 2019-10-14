487c48fb8ec7dc5a2461b49c61e788680f538d19
Aylin Meschenmoser <aylin.meschenmoser@netzwerk-p.com>
2019-10-14 16:25:38 +0200

    add skills for the SkillFilter.vue and adjust margin for desktop / tablet

---
+++ src/components/SkillFilter.vue
@@ -0,0 +1,314 @@
+<template>
+  <div class="wp-skill-filter">
+    <div class="wp-filter__searches wp-filter-searches">
+      <input
+        class="wp-filter-searches__search"
+        placeholder="Suchen" />
+      <button class="wp-filter-searches__delete">Filter löschen</button>
+    </div>
+    <ul class="wp-filters">
+      <li
+        class="wp-filters__category wp-filter-category"
+        v-for="filter in skillFilters"
+        :key="filter.category">
+        <span @click="toggleFilter(filter)">
+          <span class="wp-filter-category__label">{{ filter.category }}</span>
+          <i class="wp-filter-category__icon fa-caret-right"></i>
+        </span>
+        <ul class="wp-filter-category__skills wp-filter-skills"
+          :class="{
+            'wp-filter-skills--active': !!filter.active
+          }">
+          <li
+            class="wp-filter-skills__item wp-filter-skills-item"
+            v-for="skill in filter.skills"
+            :key="skill">
+            <input type="checkbox" class="wp-filter-skills-item__checkbox">{{ skill }}
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SkillFilter',
+
+  data() {
+    return {
+      skillFilters: [{
+        active: false,
+        category: 'Arbeitstools',
+        skills: [
+          'Bilddatenbanken',
+          'CLM-Portal',
+          'CMS',
+          'easyJob',
+          'Eclipse',
+          'InCatalog',
+          'Kanbanboard',
+          'Konfigurator',
+          'MAM',
+          'Mellow Colour',
+          'One2Edit',
+          'Slack',
+          'Trello',
+          'Wyfiles',
+          'XML',
+        ]
+      }, {
+        active: false,
+        category: 'Beratung',
+        skills: [
+          'Kundenberatung',
+          'Kundenpräsentation',
+          'Schulung / Workshop',
+          'strategische Beratung'
+        ]
+      }, {
+        active: false,
+        category: 'Bildbearbeitung',
+        skills: [
+          'CGI',
+          'Colormangement',
+          'Packaging',
+          'Retusche'
+        ]
+      }, {
+        active: false,
+        category: 'EDV-Kenntnisse',
+        skills: [
+          'Adobe Acrobat Pro',
+          'Adobe After Effects',
+          'Adobe Bridge',
+          'Adobe Encore',
+          'Adobe Illustrator AI',
+          'Adobe InDesign InDD',
+          'Adobe Lightroom',
+          'Adobe Photoshop',
+          'Adobe Premiere Pro',
+          'Adobe Suite',
+          'CAD',
+          'Cheetah',
+          'Cinema 4D',
+          'Datev',
+          'Enfocus PitStop',
+          'Excel',
+          'Garageband',
+          'Joomla',
+          'Logic Pro Audio',
+          'MAC OS',
+          'Outlook',
+          'PowerPoint PPT',
+          'SAP',
+          'Selenium Plugin',
+          'Sketch/Book',
+          'Takondi',
+          'Visio',
+          'Word',
+        ]
+      }, {
+        active: false,
+        category: 'Fotografie',
+        skills: [
+          'Allgemein',
+          'Food',
+          'People',
+          'Produkte',
+        ]
+      }, {
+        active: false,
+        category: 'Fremdsprachen',
+        skills: [
+          'Englisch',
+          'Französisch',
+          'Griechisch',
+          'Italienisch',
+          'Niederländisch',
+          'Russisch',
+          'Spanisch',
+        ]
+      }, {
+        active: false,
+        category: 'Gestaltung / Datenerstellung',
+        skills: [
+          'Appdesign',
+          'CI-Konform',
+          'Druckvorstufe',
+          'Film',
+          'Layout',
+          'Online-Medien',
+          'Packaging',
+          'Postproduction',
+          'RZ/Satz',
+          'Scribble',
+          'Typografie',
+          'Webdesign',
+        ]
+      }, {
+        active: false,
+        category: 'Marketing',
+        skills: [
+          'Marketing allgemein',
+          'Onlinemarketing',
+          'Personalmarketing',
+        ]
+      }, {
+        active: false,
+        category: 'nP Skills',
+        skills: [
+          'Ausbilder',
+          'Ideenfindung/Brainstorming',
+          'Konzeption',
+          'Lektorat/Korrektorat',
+          'Mentoring',
+          'NewBusiness',
+          'Organisation/Struktur',
+          'Schulung/Workshop',
+          'Social-Media-Plattformen (Arbeitstools)',
+          'Technischer Support',
+          'Text/Formulierung/Gestaltung',
+          'Unternehmensberatung',
+        ]
+      }, {
+        active: false,
+        category: 'nP Tools',
+        skills: [
+          'Checkflow',
+          'Imageflow',
+          'Mediaflow',
+          'Onlineshop Magento',
+          'Orderflow',
+          'Statusflow',
+        ]
+      }, {
+        active: false,
+        category: 'Produkte nP',
+        skills: [
+          '[nP]analytics',
+          '[nP]connect',
+          '[nP]creator',
+          '[nP]enriched media',
+          '[nP]pocket',
+          '[nP]vision',
+        ]
+      }, {
+        active: false,
+        category: 'Programmierung',
+        skills: [
+          'Chili',
+          'CMS',
+          'CSS',
+          'HTML',
+          'Javascript',
+          'PHP',
+          'Wordpress',
+        ]
+      }, {
+        active: false,
+        category: 'Projektmanagement',
+        skills: [
+          'Allgemein',
+          'Digitale Medien',
+          'Druck-/Werbetechnik',
+          'Einkauf',
+          'Give-Aways',
+          'Logistik',
+          'Mailing',
+          'Messe & Event',
+          'Multichannel-Management',
+          'POS/3D-Werbemittel',
+          'QMS',
+        ]
+      }, {
+        active: false,
+        category: 'Softskills',
+        skills: [
+          'Einfühlungsvermögen (Personal)',
+          'Konfliktmanagement',
+          'Meditation',
+        ]
+      }, {
+        active: false,
+        category: 'Video',
+        skills: [
+          'Equipment',
+          'Produktion',
+          'Schnitt',
+        ]
+      }]
+    };
+  },
+  methods: {
+    toggleFilter(filter) {
+      filter.active = !filter.active;
+    }
+  },
+  created() {
+
+  }
+};
+
+
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/variables';
+
+.wp-filter-searches {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 2rem;
+
+  &__search {
+    height: 2rem;
+    border-radius: 10px;
+    padding: 0.5rem;
+    margin: 1rem 0 0.5rem 0;
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &__delete {
+    color: $color-text-active;
+    text-align: left;
+    border: none;
+    background-color: white;
+
+  }
+}
+
+.wp-filter-category {
+  list-style: none;
+  margin-bottom: 1rem;
+
+  &__label {
+    color: $color-text-active;
+    font-weight: 500;
+    margin-right: 1rem;
+  }
+
+  &__icon {
+    color: $color-text-active;
+  }
+}
+
+.wp-filter-skills {
+  margin-top: 0.5rem;
+  list-style: none;
+  font-size: 0.8rem;
+}
+
+.wp-filter-skills-item {
+  &__checkbox {
+    margin-right: 0.5rem;
+  }
+}
+
+.wp-skill-filter {
+  background-color: white;
+  padding: 1rem;
+}
+</style>
--- src/views/Skills.vue
+++ src/views/Skills.vue
@@ -1,33 +1,54 @@
 <template>
   <div class="wp-skills">
-    <div class="wp-skills__skillBox wp-skillBox-container">
-      <skill-box box-title="Skills" class="wp-skillBox-container__item"/>
-    </div>
-    <div class="wp-skills__skillBoxMobile wp-skillBoxMobile-container">
-      <skill-box-mobile box-title="Skills" class="wp-skillBoxMobile-container__item"/>
+    <skill-filter class="wp-skills__filters" />
+    <div class="wp-skills__results">
+      <skill-box
+        v-for="employee in employees"
+        :key="employee.id"
+        :employee="employee"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import SkillFilter from '../components/SkillFilter.vue';
 import SkillBox from '../components/SkillBox.vue';
-import SkillBoxMobile from '../components/SkillBoxMobile.vue';
 
 export default {
-  name: 'wp-skills',
+  name: 'SkillsView',
 
   components: {
-    SkillBox,
-    SkillBoxMobile
+    SkillFilter,
+    SkillBox
   },
 
   data() {
     return {
+      employees: [{
+        id: 1,
+        name: 'Tanja Gmelin',
+        department: 'Technik',
+        phone: '-204',
+        email: 'tanja.gmelin@netzwerk-p.com'
+      }, {
+        id: 2,
+        name: 'Marius Hetterich',
+        department: 'Projekte',
+        phone: '-214',
+        email: 'tanja.gmelin@netzwerk-p.com'
+      }, {
+        id: 3,
+        name: 'Lena Beßler',
+        department: 'Technik',
+        phone: '-204',
+        email: 'tanja.gmelin@netzwerk-p.com'
+      }]
     };
   },
 
   created() {
-    console.log('wp-skills::created()');
+    console.log('Skills::created()');
   }
 };
 </script>
@@ -45,18 +66,24 @@ export default {
   @media screen and (min-width: $breakpoint-desktop) {
     margin-left: 10rem;
     margin-right: 10rem;
+    display: flex;
+    flex-direction: row;
+  }
+
+  @media screen and (max-width: $breakpoint-tablet) {
+    display: flex;
+    flex-direction: row;
+  }
+
+  &__filters {
+    @media screen and (min-width: $breakpoint-tablet) {
+      margin-right: 2rem;
+    }
   }
 
   &__skillBox {
     display: none;
     @media screen and (min-width: $breakpoint-desktop) {
-      display: block;
-    }
-  }
-
-  &__skillBoxMobile {
-    display: none;
-    @media screen and (max-width: $breakpoint-tablet) {
       display: block;
     }
   }
